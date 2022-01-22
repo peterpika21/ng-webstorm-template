c6c82dd5c6ccabf3f14a8e3115a36c1d0f9d875d
I-CHIEN <peterpika21@gmail.com>
2022-01-22 15:31:06 +0800

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ng-webstorm-template';
+  title =
+    "ng-webstorm-teplateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-templateng-webstorm-template";
 }
